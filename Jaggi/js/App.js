28949bfc028db8f5f6e3630ed86cf2d0b790bedd
adr420 <aditya000000@gmail.com>
2023-05-31 15:32:31 +0530

    Added game -> Jaggi.

---
+++ Jaggi/js/App.js
@@ -0,0 +1,9 @@
+import { Game } from "./Game.js"
+
+const cnvs = document.querySelector("[data-screen]");
+
+const App = {
+	init: function(){
+		
+	}
+};
---
+++ Jaggi/js/Game.js
@@ -0,0 +1,7 @@
+
+
+export const Game = {
+	init: function(){
+		
+	}
+};
